895eddda1606bdc0415609da5c1b30ca711fc3a2
klemminn <klemenz88@gmail.com>
2020-02-08 18:45:17 +0000

    Updates

---
+++ precache-manifest.121f5a8d8770dac3f396ab36d0b11e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "369d0c37eea646c0a183",
+    "url": "/davidingvi.com/static/css/main.9cd40a7b.chunk.css"
+  },
+  {
+    "revision": "369d0c37eea646c0a183",
+    "url": "/davidingvi.com/static/js/main.369d0c37.chunk.js"
+  },
+  {
+    "revision": "864b1478313b56a24d69",
+    "url": "/davidingvi.com/static/js/runtime~main.864b1478.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/davidingvi.com/static/js/2.477d3cfd.chunk.js"
+  },
+  {
+    "revision": "177fcdc473c29e7e236e0d4b95e9576c",
+    "url": "/davidingvi.com/index.html"
+  }
+];
