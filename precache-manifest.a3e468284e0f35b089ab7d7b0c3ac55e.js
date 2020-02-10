a814deb76b5107ccd5119d274f9206af932d0a86
klemminn <klemenz88@gmail.com>
2020-02-10 22:10:53 +0000

    Updates

---
+++ precache-manifest.a3e468284e0f35b089ab7d7b0c3ac55e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d21c5fe632b62c5831b7",
+    "url": "/static/css/main.9cd40a7b.chunk.css"
+  },
+  {
+    "revision": "d21c5fe632b62c5831b7",
+    "url": "/static/js/main.d21c5fe6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.477d3cfd.chunk.js"
+  },
+  {
+    "revision": "f20e313519d96c47217bbf5e2e9cfd6a",
+    "url": "/index.html"
+  }
+];
